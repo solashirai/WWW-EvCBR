1f1741383f857116d86663c049a47c536be08b61
solashirai <solashakashirai@gmail.com>
2023-02-12 22:09:38 -0500

    change plural name to singular

--- case_support.py
+++ case_support.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from typing import Dict, Set, Tuple, List
 from rdflib import URIRef
-from similar_cause_effect_choices import SimilarCauseEffectChoices
+from similar_cause_effect_choice import SimilarCauseEffectChoice
 
 
 @dataclass
 class CaseSupport:
     property_entity_support: Dict[URIRef, Dict[URIRef, float]]
-    similar_cause_effect_pairs: List[SimilarCauseEffectChoices]
+    similar_cause_effect_pairs: List[SimilarCauseEffectChoice]
     c_to_e_paths: Set[Tuple[Tuple[URIRef, str]]]
 
     def sorted_property_prediction(self, *, property_uri: URIRef) -> List[Tuple[URIRef, float]]:
